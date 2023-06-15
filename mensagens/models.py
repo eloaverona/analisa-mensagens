29150d59980e65390b0c998f59390161a540def7
Caju França <caju.barro@proton.me>
2023-06-15 09:53:07 -0300

    Adiciona métodos para transformar Mensagem em JSON
    Adiciona métodos para serializar e deserializa
    uma mensagem de uma string JSON

--- mensagens/models.py
+++ mensagens/models.py
@@ -1,3 +1,8 @@
+import uuid
+import json
+import jsonschema
+from datetime import date
+from jsonschema import Draft202012Validator
 from django.db import models
 
 
@@ -16,3 +21,60 @@ class Mensagem(models.Model):
     status = models.CharField(max_length=200)
     texto = models.TextField()
 
+    def fromJSON(jsonData):
+        """Deserializa uma string JSON em uma Mensagem
+
+            Args:
+                jsonData: uma mensagen serializada como string JSON.
+            Returns:
+                Uma instância de Mensagem deserializada da string JSON.
+            Raises:
+                ValueError: caso a string JSON fornecida não seja uma mensagem
+                válida.
+                Exception: caso ocorra um erro ao deserializar a string JSON.
+        """
+        mensagemSchema = {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "type": "object",
+            "properties": {
+                "data": {"type": "string", "format": "date"},
+                "status": {"type": "string"},
+                "texto": {"type": "string"},
+                "id": {"type": "number"},
+            },
+            "required": ["data", "status", "texto"],
+        }
+        try:
+            message = json.loads(jsonData)
+            jsonschema.validate(
+                message, mensagemSchema,
+                format_checker=Draft202012Validator.FORMAT_CHECKER)
+        except jsonschema.exceptions.ValidationError as error:
+            raise ValueError(
+                "Os dados JSON não são uma mensagem válida: {} ".format(
+                    error.message))
+        except Exception as error:
+            raise Exception(
+                "A tentativa de validar os dados JSON falhou: {}".format(
+                    error))
+        messageDate = date.fromisoformat(message["data"])
+        newMessage = Mensagem(
+            data=messageDate, status=message["status"],
+            texto=message["texto"])
+        if "id" in message:
+            newMessage.id = uuid.UUID(int=message["id"])
+        return newMessage
+
+    def toJSON(self):
+        """Serializa a mensagem para formato de string JSON"""
+
+        mensagem = {
+            "id": self.id.int,
+            "data": self.data.strftime("%Y-%m-%d"),
+            "status": self.status,
+            "texto": self.texto
+        }
+        return json.dumps(mensagem)
+
+    def __str__(self):
+        return self.toJSON()
--- mensagens/tests.py
+++ mensagens/tests.py
@@ -1,2 +1,67 @@
 from django.test import TestCase
+import json
+from .models import Mensagem
+from datetime import date
+from jsonschema.exceptions import ValidationError
+
+
+class MensagemModelTests(TestCase):
+    def test_mensagem_deserializes_well_formed_json(self):
+        """Verifica se o método Mensagem.fromJSON deserializa corretamente
+        uma string JSON formatada corretamente.
+        """
+        dados = """{"data": "2001-02-11", "status": "Em espera", "texto":
+                "Estou bem chateado. Você poderia nos mandar seus dados?"}"""
+        mensagem = Mensagem.fromJSON(dados)
+        self.assertEqual(mensagem.data, date.fromisoformat("2001-02-11"))
+        self.assertEqual(mensagem.status, "Em espera")
+        self.assertEqual(
+            mensagem.texto,
+            "Estou bem chateado. Você poderia nos mandar seus dados?")
+
+    def test_mensagem_raises_error_bad_formed_json(self):
+        """Verifica que o método Mensagem.fromJSON levanta um erro caso
+        o input é uma string JSON mal formatada
+        """
+        dados = """{"data": "Não sou uma data", "status":
+            "Em espera", "texto":
+            "Estou bem chateado. Você poderia nos mandar seus dados?"}"""
+        try:
+            Mensagem.fromJSON(dados)
+        except ValidationError:
+            pass
+        except ValueError:
+            pass
+        except Exception:
+            pass
+        else:
+            self.fail("Erro inesperado aconteceu")
+
+    def test_mensagem_serializes_well_formed_json(self):
+        """Verifica se o método toJSON serializa corretamente
+        uma string.
+        """
+        mensagem = Mensagem(
+            data=date.fromisoformat("2001-02-11"), status="Em Espera",
+            texto="Estou bem chateado. Gostaria de fazer um pedido.")
+        dadosJSON = mensagem.toJSON()
+        mensagemJSON = json.loads(dadosJSON)
+        self.assertEqual(
+            mensagemJSON["data"], "2001-02-11")
+        self.assertEqual(mensagemJSON["status"], "Em Espera")
+        self.assertEqual(
+            mensagemJSON["texto"],
+            "Estou bem chateado. Gostaria de fazer um pedido.")
+
+    def test_mensagem_serializes_deserializes(self):
+        """Verifica que uma mensagem serializado método toJSON, pode ser
+        deserializada pelo método fromJSON.
+        """
+        mensagemOriginal = Mensagem(
+            data=date.fromisoformat("2001-02-11"), status="Em Espera",
+            texto="Estou bem chateado. Gostaria de fazer um pedido.")
+        dadosJSON = mensagemOriginal.toJSON()
+        mensagemDeserializada = Mensagem.fromJSON(dadosJSON)
+        self.assertEqual(
+            mensagemOriginal, mensagemDeserializada)
 
