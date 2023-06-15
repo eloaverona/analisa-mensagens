104f3f9576012dcdf98671c0fc6f67af62b1a1cb
Caju França <caju.barro@proton.me>
2023-06-15 09:45:08 -0300

    Adiciona modelo e tabela para mensagens
    Adiciona um modelo que representa uma mensagem e
    sua tabela no banco de dados

--- mensagens/models.py
+++ mensagens/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+
+class Mensagem(models.Model):
+    """ Modelo de uma Mensagem no banco de dados
+
+    Attributes:
+        id: uma UUID que identifica unicamente uma mensagem.
+        data: data em formato YYYY-mm-dd
+        status: o status da mensagem
+        texto: o texto da mensagem
+
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    data = models.DateField()
+    status = models.CharField(max_length=200)
+    texto = models.TextField()
+
