f7ae1f411fcf5d10ad4924782a48ca863f3eb674
Caju França <caju.barro@proton.me>
2023-06-15 09:56:22 -0300

    Adiciona funções para realizar operações CRUD
    Adiciona funções para inserir, listar, atualizar e
    deletar mensagens ao banco de dados. Essas funções
    são wrappers ao redor dos métodos nativos do Django para
    realizar operações CRUDS. Elas recebem como input dados
    em formato JSON string e retornam dados em formato JSON string,
    para facilitar a comunicação desses dados via RESTapi.

---
+++ mensagens/database_handler.py
@@ -0,0 +1,113 @@
+from .models import Mensagem
+import json
+
+
+def insertMessage(newMessage):
+    """Adiciona uma nova mensagem ao banco de dados
+
+    Args:
+        newMessage: A mensagem a ser adicionada serializada no formato de string
+        JSON
+    Raises:
+        Exception: Um erro ocorreu ao deserializar o JSON ou ao se comunicar
+        com o banco de dados
+    """
+    try:
+        message = Mensagem.fromJSON(newMessage)
+    except Exception as error:
+        raise Exception(
+            "Falha ao criar mensagem. Verifique o formato do input: {}"
+            .format(error))
+    try:
+        message.save()
+    except Exception as error:
+        raise Exception(
+            "Erro ao adicionar mensagem no banco de dados: {}".format(error))
+
+
+def fetchMessage(messageID):
+    """Pega uma mensagem específica no banco de dados
+
+    Args:
+        messageID: a id única da mensagem
+    Returns:
+        A mensagem serializada em formato de string JSON
+    Raises:
+        Exception: uma mensagem com a message_id fornecida não existe ou caso
+     houver uma falha ao acessar o banco de dados
+    """
+    try:
+        message = Mensagem.objects.get(pk=messageID)
+    except Mensagem.DoesNotExist:
+        raise Exception("Messagem com id {} não existe".format(message_id))
+    except Exception as error:
+        raise Exception("Erro ao acessar o banco de dados: {}".format(error))
+    return message.toJSON()
+
+
+def listMessages():
+    """Retorna uma lista com todas as mensagens no banco de dados
+
+    Returns:
+        Uma lista com as mensagens serializada em formato de string JSON
+    Raises:
+        Exception: caso houver uma falha ao acessar o banco de dados
+    """
+    try:
+        messages = list(Mensagem.objects.all())
+        messages = list(map(lambda m: m.toJSON(), messages))
+        messages = ", ".join(messages)
+        messages = "[" + messages + "]"
+    except Exception as error:
+        raise Exception("Erro ao acessar o banco de dados: {}".format(error))
+    return messages
+
+
+def deleteMessage(messageID):
+    """Deleta uma mensagem específica do banco de dados
+
+    Args:
+        messageID: a id única da mensagem a ser deletada
+    Returns:
+        A mensagem que foi deletada serializada em formato de string JSON
+    Raises:
+        Exception: uma mensagem com a message_id fornecida não existe ou caso
+        houver uma falha ao acessar o banco de dados
+    """
+
+    try:
+        message = Mensagem.objects.get(pk=messageID)
+        jsonEncodedMessage = message.toJSON()
+        message.delete()
+    except Mensagem.DoesNotExist:
+        raise Exception("Messagem com id {} não existe".format(message_id))
+    except Exception as error:
+        raise Exception("Erro ao acessar o banco de dados: {}".format(error))
+    return jsonEncodedMessage
+
+def updateMessage(messageID, jsonData):
+    """Atualiza uma mensagem específica no banco de dados
+
+    Args:
+        messageID: a id única da mensagem a ser atualizada
+        jsonData: a mensagem com os dados atualizados serializados em formato
+        de string JSON
+    Returns:
+        A mensagem serializada em formato de string JSON
+    Raises:
+        Exception: uma mensagem com a message_id fornecida não existe ou caso
+        houver uma falha ao acessar o banco de dados
+    """
+    try:
+        message = Mensagem.objects.get(pk=messageID)
+        updatedMessage = Mensagem.fromJSON(jsonData)
+        message.status = updatedMessage.status
+        message.texto = updatedMessage.texto
+        message.data = updatedMessage.data
+        message.save()
+    except Mensagem.DoesNotExist:
+        raise Exception("Messagem com id {} não existe".format(message_id))
+    except ValueError as error:
+        raise Exception("Os dados do JSON não são uma mensagem válida: {}".format(error))
+    except Exception as error:
+        raise Exception("Erro ao acessar o banco de dados: {}".format(error))
--- mensagens/tests.py
+++ mensagens/tests.py
@@ -3,6 +3,7 @@ import json
 from .models import Mensagem
 from datetime import date
 from jsonschema.exceptions import ValidationError
+import mensagens.database_handler as dbHandler
 
 
 class MensagemModelTests(TestCase):
@@ -65,3 +66,152 @@ class MensagemModelTests(TestCase):
         self.assertEqual(
             mensagemOriginal, mensagemDeserializada)
 
+
+class DatabaseHandlerTests(TestCase):
+    def test_insert_mensagem_success(self):
+        """Verifica que é possível adicionar uma mensagem no banco de dados
+        com sucesso
+        """
+        dados = """{"data": "2001-02-11", "status": "Em espera", "texto":
+                "Estou bem chateado. Você poderia nos mandar seus dados?"}"""
+        dbHandler.insertMessage(dados)
+        mensagens = list(Mensagem.objects.all())
+        self.assertEqual(1, len(mensagens))
+        mensagem = mensagens[0]
+        self.assertEqual(mensagem.data, date.fromisoformat("2001-02-11"))
+        self.assertEqual(mensagem.status, "Em espera")
+        self.assertEqual(
+            mensagem.texto,
+            "Estou bem chateado. Você poderia nos mandar seus dados?")
+
+    def test_fetch_mensagem_success(self):
+        """Verifica que o método fetchMessage retorna a mensagem esperada"""
+        self.seed_mock_data_to_database()
+        mensagens = list(Mensagem.objects.all())
+        self.assertEqual(3, len(mensagens))
+        mensagemID = mensagens[0].id
+        mensagemJSON = dbHandler.fetchMessage(mensagemID)
+        mensagem = json.loads(mensagemJSON)
+        self.assertEqual(mensagem["id"], mensagens[0].id.int)
+        self.assertEqual(
+            mensagem["data"],
+            mensagens[0].data.strftime("%Y-%m-%d"))
+        self.assertEqual(mensagem["status"], mensagens[0].status)
+        self.assertEqual(mensagem["texto"], mensagens[0].texto)
+
+    def test_fetch_mensagem_wrong_id(self):
+        """Verifica que o método fetchMessage retorna um erro se uma id que não
+            existe no banco de dados for fornecida
+        """
+        self.seed_mock_data_to_database()
+        mensagens = list(Mensagem.objects.all())
+        self.assertEqual(3, len(mensagens))
+        try:
+            dbHandler.fetchMessage(14545)
+        except Exception:
+            pass
+        else:
+            self.fail("Erro inesperado aconteceu")
+
+    def test_list_mensagens_success(self):
+        """Verifica que o método listMessages retorna uma lista de mensagens
+        em formato string de JSON
+        """
+        self.seed_mock_data_to_database()
+        mensagens = list(Mensagem.objects.all())
+        self.assertEqual(3, len(mensagens))
+        mensagensJSON = dbHandler.listMessages()
+        mensagensDB = json.loads(mensagensJSON)
+        self.assertEqual(3, len(mensagensDB))
+
+        self.assertEqual(mensagensDB[0]["id"], mensagens[0].id.int)
+        self.assertEqual(
+            mensagensDB[0]["data"],
+            mensagens[0].data.strftime("%Y-%m-%d"))
+        self.assertEqual(
+            mensagensDB[0]["status"],
+            mensagens[0].status)
+        self.assertEqual(
+            mensagensDB[0]["texto"],
+            mensagens[0].texto)
+
+        self.assertEqual(mensagensDB[1]["id"], mensagens[1].id.int)
+        self.assertEqual(
+            mensagensDB[1]["data"],
+            mensagens[1].data.strftime("%Y-%m-%d"))
+        self.assertEqual(
+            mensagensDB[1]["status"],
+            mensagens[1].status)
+        self.assertEqual(
+            mensagensDB[1]["texto"],
+            mensagens[1].texto)
+
+        self.assertEqual(mensagensDB[2]["id"], mensagens[2].id.int)
+        self.assertEqual(
+            mensagensDB[2]["data"],
+            mensagens[2].data.strftime("%Y-%m-%d"))
+        self.assertEqual(
+            mensagensDB[2]["status"],
+            mensagens[2].status)
+        self.assertEqual(
+            mensagensDB[2]["texto"],
+            mensagens[2].texto)
+
+    def test_update_mensagem_success(self):
+        """Verifica que o método updateMessage atualiza a mensagem esperada"""
+        self.seed_mock_data_to_database()
+        mensagens = list(Mensagem.objects.all())
+        self.assertEqual(3, len(mensagens))
+        mensagemID = mensagens[0].id
+        newStatus = "Atualizado"
+        newText = "Mensagem atualizada"
+        updatedMessage = Mensagem(
+            id=mensagemID,
+            data=mensagens[0].data,
+            status=newStatus,
+            texto=newText)
+        jsonMessage = updatedMessage.toJSON()
+        dbHandler.updateMessage(mensagemID, jsonMessage)
+
+        mensagem = Mensagem.objects.get(pk=mensagemID)
+        self.assertEqual(mensagem.id, mensagens[0].id)
+        self.assertEqual(mensagem.data, mensagens[0].data)
+        self.assertNotEqual(mensagem.status, mensagens[0].status)
+        self.assertNotEqual(mensagem.texto, mensagens[0].texto)
+
+        self.assertEqual(mensagem.status, newStatus)
+        self.assertEqual(mensagem.texto, newText)
+
+    def test_delete_mensagem_success(self):
+        """Verifica que o método deleteMessage deleta a mensagem esperada"""
+        self.seed_mock_data_to_database()
+        mensagens = list(Mensagem.objects.all())
+        self.assertEqual(3, len(mensagens))
+        mensagemID = mensagens[0].id
+        mensagemJSON = dbHandler.deleteMessage(mensagemID)
+        self.assertEqual(2, len(list(Mensagem.objects.all())))
+        mensagem = json.loads(mensagemJSON)
+        self.assertEqual(mensagem["id"], mensagens[0].id.int)
+        self.assertEqual(
+            mensagem["data"],
+            mensagens[0].data.strftime("%Y-%m-%d"))
+        self.assertEqual(mensagem["status"], mensagens[0].status)
+        self.assertEqual(mensagem["texto"], mensagens[0].texto)
+
+    def seed_mock_data_to_database(self):
+        mensagem1 = Mensagem(
+            data="2022-01-24", status="Em Espera",
+            texto="Estou bem chateado. Gostaria de fazer um pedido.")
+
+        mensagem2 = Mensagem(
+            data="2021-02-23", status="Aberto",
+            texto="""Gostaria de fazer um pedido. Preciso que resolvam um
+            problema.""")
+
+        mensagem3 = Mensagem(
+            data="2020-05-19", status="Fechado",
+            texto="ótima empresa. Olá, como vai?")
+
+        mensagem1.save()
+        mensagem2.save()
+        mensagem3.save()
